b915df006938897c7cdee51c2a77f23e8d554453
Jeremy G.Lafleur <010JGL@gmail.com>
2023-02-11 02:33:55 -0500

    How to find the longest value in an object inside an array

---
+++ kata-four.js
@@ -0,0 +1,21 @@
+const instructorWithLongestName = function(instructors) {
+  let longest = instructors[0].name     // Start at the first name in the array
+  for (let i = 0; i < instructors.length; i++) {      // loop to check the whole array
+    if (instructors[i].name.length > longest.length) {   // compares the index length with the current length
+      longest = instructors[i];         // when it is longer, changes the value of longest
+    }
+  }
+  return longest;                       // return the value out of the function
+};
+
+console.log(instructorWithLongestName([
+  {name: "Samuel", course: "iOS"},
+  {name: "Jeremiah", course: "Web"},
+  {name: "Ophilia", course: "Web"},
+  {name: "Donald", course: "Web"}
+]));
+console.log(instructorWithLongestName([
+  {name: "Matthew", course: "Web"},
+  {name: "David", course: "iOS"},
+  {name: "Domascus", course: "Web"}
+]));
