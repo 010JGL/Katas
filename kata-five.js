229aaba885e5943c5610d5767a5bb28828ed46c0
Jeremy G.Lafleur <010JGL@gmail.com>
2023-02-12 15:36:25 -0500

    How to change spaces in a string for another character, using a function

---
+++ kata-five.js
@@ -0,0 +1,16 @@
+const urlEncode = function(text) {
+  var string = []   // creat an empty array
+  for (let i = 0; i < text.length; i++) {  // loop thru text one character at the time
+    if (text[i] === " ") { // if character is space
+    string += "%20";   // change it to %20
+  } else {
+    string += (text[i]);  //if not, add the original text index character
+  }
+
+}
+  return string;  // returns value out of the function
+};
+
+console.log(urlEncode("Lighthouse Labs"));
+console.log(urlEncode(" Lighthouse Labs "));
+console.log(urlEncode("blue is greener than purple for sure"));
