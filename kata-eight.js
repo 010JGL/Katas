f4b896fcbe3070607f70588b93feee8247926398
Jeremy G.Lafleur <010JGL@gmail.com>
2023-02-15 16:40:10 -0500

    How to add a value from an array the number of times indicated in the same array

---
+++ kata-eight.js
@@ -0,0 +1,25 @@
+const repeatNumbers = function(data) {
+  let result = "";   // for the single value in the loop
+  let finalResult = [];  // values when added together in an array
+
+  for (let x = 0; x < data.length; x++) {  // loop for the arrays
+    for (let y = 0; y < data[x][1]; y++) { // loop for values inside the arrays
+      
+      result = result + data[x][0];    // adds the first value to repeat
+    
+      }
+    finalResult.push(result);    // adds elements inside an array
+    result = "";  // reset the result to initial value after a loop
+    }
+  return finalResult.join(", ");  // concat all the values and split them with ", "
+  
+    
+  }    
+  
+
+
+console.log(repeatNumbers([[1, 10]]));
+console.log(repeatNumbers([[1, 2], [2, 3]]));
+console.log(repeatNumbers([[10, 4], [34, 6], [92, 2]]));
+
+
