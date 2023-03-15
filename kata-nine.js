0c80f4385931169a63371dc7357078e1a34f4c81
Jeremy <010JGL@GMAIL.COM>
2023-03-14 23:38:27 -0400

    Comments and eslint

--- kata-nine.js
+++ kata-nine.js
@@ -1,20 +1,21 @@
 const camelCase = function(input) {
-  let newStr = "";  // Create a new string
+  let newStr = "";                         // Create a new string
+  
   if (input) {
-    let wordArr = input.split(/[ ]/); // split a string into an array, when there is a space. Called wordArr
-    for (let i in wordArr) {  // creates a loop in the array
-      if (i > 0) {  // Starts with 2nd word, we dont need first word capitalized
+    let wordArr = input.split(/[ ]/);     // split a string into an array, when there is a space. Called wordArr
+    for (let i in wordArr) {              // creates a loop in the array
+      if (i > 0) {                        // Starts with 2nd word, we dont need first word capitalized
         newStr += wordArr[i].charAt(0).toUpperCase() + wordArr[i].slice(1);  // we add the words together, charAt says the first letter of the word. Uppercase is used to capitalize
         
       } else {
-        newStr += wordArr[i]  //if its the first word we dont capitalize
+        newStr += wordArr[i];              //if its the first word we dont capitalize
       }
-    } 
+    }
   } else {
-    return newStr; // returns newStr out of the loop
-   }
-  return newStr; // returns newStr out of the function
-}
+    return newStr;                        // returns newStr out of the loop
+  }
+  return newStr;                          // returns newStr out of the function
+};
 
 console.log(camelCase("this is a string"));
 console.log(camelCase("loopy lighthouse"));
--- kata-seven.js
+++ kata-seven.js
@@ -1,16 +1,15 @@
-const checkAir = function (samples, threshold) {
-  let count = 0;      // create a variable to start counting at 0
+const checkAir = function(samples, threshold) {
+  let count = 0;                              // create a variable to start counting at 0
     
   for (let i = 0; i < samples.length; i++) {  // loop to find the number of dirty in the array
-    if (samples[i] === 'dirty') {
-      count++;            // adds one for every time
+    if (samples[i] === 'dirty') {              // if the index is === value
+      count++;                                // adds one for every time
     }
   }
   if ((count / samples.length) >= threshold) {   // check if condition is met, outside of the loop. count is divided by the length for the right value
     return "Polluted";
-  }
-    else {
-      return "Clean";
+  } else {
+    return "Clean";
   }
 };
 
@@ -27,4 +26,4 @@ console.log(checkAir(
 console.log(checkAir(
   ['clean', 'dirty', 'clean', 'dirty', 'clean', 'dirty', 'clean'],
   0.9
-))
+));
