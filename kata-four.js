779bc4e171f1bf06e73d0ac7ec1b717aafb24c56
Jeremy <010JGL@GMAIL.COM>
2023-03-14 23:29:35 -0400

    Comments and eslint

--- kata-four.js
+++ kata-four.js
@@ -1,11 +1,12 @@
 const instructorWithLongestName = function(instructors) {
-  let longest = instructors[0].name     // Start at the first name in the array
-  for (let i = 0; i < instructors.length; i++) {      // loop to check the whole array
+  let longest = instructors[0].name;                     // Start at the first name in the array
+  
+  for (let i = 0; i < instructors.length; i++) {        // loop to check the whole array
     if (instructors[i].name.length > longest.length) {   // compares the index length with the current length
-      longest = instructors[i];         // when it is longer, changes the value of longest
+      longest = instructors[i];                         // when it is longer, changes the value of longest
     }
   }
-  return longest;                       // return the value out of the function
+  return longest;                                        // return the value out of the function
 };
 
 console.log(instructorWithLongestName([
--- kata-one.js
+++ kata-one.js
@@ -1,21 +1,20 @@
 const sumLargestNumbers = function(data) {
   let first = 0;
-  let second = 0;  // declare values for the condition IF
+  let second = 0;                                 // declare values for the condition IF
   
-  for (let i = 0; i < data.length; i++) {  // Loop thru data
+  for (let i = 0; i < data.length; i++) {         // Loop thru data
     if (data[i] > first && data[i] !== second) { // if data index is bigger AND is not the same as second
-      first = data[i]                             // first = data index and loops again
+      first = data[i];                             // first = data index and loops again
     }
   }
   for (let j = 0; j < data.length; j++) {         // second loop for second number
     if (data[j] > second && data[j] !== first) {  // checks for data bigger than second but not the same as first
-      second = data[j]                            // register the higest value in second
+      second = data[j];                            // register the higest value in second
     }
   }
-  return first + second;           // add both together for total
+  return first + second;                          // add both together for total
   
 };
-
 
 
 console.log(sumLargestNumbers([1, 10]));
